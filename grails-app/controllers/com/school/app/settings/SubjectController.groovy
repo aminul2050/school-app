ea597e978a205f7ac9021cb65a4974ae40408bc5
Hasnat <ahasnat3@gmail.com>
2014-05-31 21:25:42 +0600

    Controller Add

--- grails-app/controllers/com/school/app/settings/SubjectController.groovy
+++ grails-app/controllers/com/school/app/settings/SubjectController.groovy
@@ -1,6 +1,142 @@
 package com.school.app.settings
+import com.app.school.settings.Subject
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured(['ROLE_SUPER_ADMIN'])
 class SubjectController {
 
-    def index() { }
+    def subjectService
+
+    def index() {
+        LinkedHashMap resultMap = subjectService.subjectPaginateList(params)
+
+        if (!resultMap || resultMap.totalCount == 0) {
+            render(view: 'subject', model: [dataReturn: null, totalCount: 0])
+            return
+        }
+        int totalCount = resultMap.totalCount
+        render(view: 'subject', model: [dataReturn: resultMap.results, totalCount: totalCount])
+    }
+
+    def save(ClassNameCommand subjectCommand) {
+        if (!request.method == 'POST') {
+            redirect(action: 'index')
+            return
+        }
+        LinkedHashMap result = new LinkedHashMap()
+        result.put('isError',true)
+        String outPut
+        if (subjectCommand.hasErrors()) {
+            result.put('message','Please fill the form correctly')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        Subject subject
+        if (params.id) { //update Currency
+            subject = Subject.get(subjectCommand.id)
+            if (!subject) {
+                result.put('message','Class not found')
+                outPut=result as JSON
+                render outPut
+                return
+            }
+            subject.properties = subjectCommand.properties
+            if (!subject.validate()) {
+                result.put('message','Please fill the form correctly')
+                outPut=result as JSON
+                render outPut
+                return
+            }
+            Subject savedClass =subject.save()
+            result.put('isError',false)
+            result.put('message','Class Updated successfully')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        subject = new Subject(subjectCommand.properties)
+        if (!subjectCommand.validate()) {
+            result.put('message','Please fill the form correctly')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        Subject savedCurr = subject.save(flush: true)
+        if (!savedCurr) {
+            result.put('message','Please fill the form correctly')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        result.put('isError',false)
+        result.put('message','Class updated successfully')
+        outPut=result as JSON
+        render outPut
+
+    }
+
+    def delete(Long id) {
+        Subject subject = Subject.get(id)
+        if (!subject) {
+            // flash.message = "Currency not found"
+            //render(template: '/coreBanking/settings/currency/currencyList')
+        }
+        subject.delete(flush: true)
+
+        def result=[isError:false,message:"Subject deleted g g successfully"]
+        String outPut=result as JSON
+        render outPut
+    }
+
+    def list() {
+        LinkedHashMap gridData
+        String result
+        LinkedHashMap resultMap =subjectService.subjectPaginateList(params)
+
+        if(!resultMap || resultMap.totalCount== 0){
+            gridData = [iTotalRecords: 0, iTotalDisplayRecords: 0, aaData: null]
+            result = gridData as JSON
+            render result
+            return
+        }
+        int totalCount =resultMap.totalCount
+        gridData = [iTotalRecords: totalCount, iTotalDisplayRecords: totalCount, aaData: resultMap.results]
+        result = gridData as JSON
+        render result
+
+    }
+
+    def edit(Long id) {
+        if (!request.method == 'POST') {
+            redirect(action: 'index')
+            return
+        }
+        LinkedHashMap result = new LinkedHashMap()
+        result.put('isError',true)
+        String outPut
+        Subject subject = Subject.read(id)
+        if (!subject) {
+            result.put('message','Class name not found')
+            outPut = result as JSON
+            render outPut
+            return
+        }
+        result.put('isError',false)
+        result.put('obj',subject)
+        outPut = result as JSON
+        render outPut
+    }
+
+}
+
+class SubjectCommand {
+    Long id
+    String name
+    String description
+
+    static constraints = {
+        name nullable: false
+    }
 }
--- grails-app/domain/com/app/school/settings/Subject.groovy
+++ grails-app/domain/com/app/school/settings/Subject.groovy
@@ -6,10 +6,20 @@ class Subject {
     String description
     Integer ctMark
     Integer hallMark
+    Boolean compulsory =true
 
     static constraints = {
-        description(nullable: true)
+        name nullable: false, unique: true
+        description nullable: true
+
+        //System inserted data
+        schoolId nullable: true
+        createdBy nullable: true
+        createdOn nullable: true
+        lastUpdatedBy nullable: true
+        lastUpdatedOn nullable: true
     }
+
     //common properties for every table
     Long schoolId
     String createdBy
@@ -17,11 +27,12 @@ class Subject {
     String lastUpdatedBy
     Date lastUpdatedOn
     def beforeInsert(){
-        createdBy = springSecurityService.principal.username
+        schoolId=10000
+        createdBy = springSecurityService?.principal? springSecurityService.principal.username:'SystemUser'
         createdOn = new Date()
     }
     def beforeUpdate() {
-        lastUpdatedBy = springSecurityService.principal.username
+        lastUpdatedBy = springSecurityService?.principal? springSecurityService.principal.username:'SystemUser'
         lastUpdatedOn = new Date()
     }
     //common properties for every table
