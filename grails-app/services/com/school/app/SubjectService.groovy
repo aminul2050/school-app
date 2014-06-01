46fb1e1816032a72d0e3edd87d778db6f6515e79
aminul <aminul2050@gmail.com>
2014-06-01 21:02:20 +0600

    school

--- grails-app/services/com/school/app/SubjectService.groovy
+++ grails-app/services/com/school/app/SubjectService.groovy
@@ -1,13 +1,14 @@
 package com.school.app
 
-import com.app.school.settings.ClassName
+import com.app.school.settings.Subject
 import grails.transaction.Transactional
 import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap
 
 @Transactional
-class ClassNameService {
+class SubjectService {
+
     static final String[] sortColumns = ['id','name']
-    LinkedHashMap classNamePaginateList(GrailsParameterMap params){
+    LinkedHashMap classNamePaginateList(GrailsParameterMap  params){
         int iDisplayStart = params.iDisplayStart ? params.getInt('iDisplayStart') : CommonUtils.DEFAULT_PAGINATION_START
         int iDisplayLength = params.iDisplayLength ? params.getInt('iDisplayLength') : CommonUtils.DEFAULT_PAGINATION_LENGTH
         String sSortDir = params.sSortDir_0 ? params.sSortDir_0 : CommonUtils.DEFAULT_PAGINATION_SORT_ORDER
@@ -19,7 +20,7 @@ class ClassNameService {
         }
         String sortColumn = CommonUtils.getSortColumn(sortColumns,iSortingCol)
         List dataReturns = new ArrayList()
-        def c = ClassName.createCriteria()
+        def c = Subject.createCriteria()
         def results = c.list(max: iDisplayLength, offset: iDisplayStart) {
             and {
                 eq("schoolId", CommonUtils.DEFAULT_SCHOOL_ID)
@@ -37,13 +38,13 @@ class ClassNameService {
             if (sSortDir.equals(CommonUtils.SORT_ORDER_DESC)) {
                 serial = (totalCount + 1) - iDisplayStart
             }
-            results.each { ClassName className ->
+            results.each { Subject subject ->
                 if (sSortDir.equals(CommonUtils.SORT_ORDER_ASC)) {
                     serial++
                 } else {
                     serial--
                 }
-                dataReturns.add([DT_RowId: className.id, 0: serial, 1: className.name,2:className.description, 3: ''])
+                dataReturns.add([DT_RowId: subject.id, 0: serial, 1: subject.name,2:subject.description,3:subject.ctMark,4:subject.hallMark,5:subject.compulsory?'Compulsory':'Optional', 6: ''])
             }
         }
         return [totalCount:totalCount,results:dataReturns]
