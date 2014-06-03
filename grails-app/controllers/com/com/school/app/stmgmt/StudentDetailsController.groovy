bdf81ceb7a2f7a5c5b373839a1bbc729afb1f041
Hasnat <ahasnat3@gmail.com>
2014-06-04 02:31:28 +0600

    View page

---
+++ grails-app/controllers/com/com/school/app/stmgmt/StudentDetailsController.groovy
@@ -0,0 +1,163 @@
+package com.com.school.app.stmgmt
+
+import com.app.school.stmgmt.StudentDetails
+import grails.converters.JSON
+import grails.plugin.springsecurity.annotation.Secured
+import org.springframework.dao.DataIntegrityViolationException
+
+@Secured(['ROLE_SUPER_ADMIN'])
+class StudentDetailsController {
+
+    def studentDetailsService
+
+    def index() {
+        LinkedHashMap resultMap = studentDetailsService.studentDetailsPaginateList(params)
+
+        if (!resultMap || resultMap.totalCount == 0) {
+            render(view: 'studentDetails', model: [dataReturn: null, totalCount: 0])
+            return
+        }
+        int totalCount = resultMap.totalCount
+        render(view: 'studentDetails', model: [dataReturn: resultMap.results, totalCount: totalCount])
+    }
+
+    def save(StudentDetailsCommand studentDetailsCommand) {
+        if (!request.method == 'POST') {
+            redirect(action: 'index')
+            return
+        }
+        LinkedHashMap result = new LinkedHashMap()
+        result.put('isError',true)
+        String outPut
+        if (studentDetailsCommand.hasErrors()) {
+            result.put('message','Please fill the form correctly')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        StudentDetails studentDetails
+        if (params.id) { //update Currency
+            studentDetails = StudentDetails.get(studentDetailsCommand.id)
+            if (!studentDetails) {
+                result.put('message','Student not found')
+                outPut=result as JSON
+                render outPut
+                return
+            }
+            studentDetails.properties = studentDetailsCommand.properties
+            if (!studentDetails.validate()) {
+                result.put('message','Please fill the form correctly')
+                outPut=result as JSON
+                render outPut
+                return
+            }
+            StudentDetails savedClass =studentDetails.save()
+            result.put('isError',false)
+            result.put('message','Student Updated successfully')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        studentDetails = new StudentDetails(studentDetailsCommand.properties)
+        if (!studentDetailsCommand.validate()) {
+            result.put('message','Please fill the form correctly')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        StudentDetails savedCurr = studentDetails.save(flush: true)
+        if (!savedCurr) {
+            result.put('message','Please fill the form correctly')
+            outPut=result as JSON
+            render outPut
+            return
+        }
+        result.put('isError',false)
+        result.put('message','Student updated successfully')
+        outPut=result as JSON
+        render outPut
+
+    }
+
+    def delete(Long id) {
+        LinkedHashMap result = new LinkedHashMap()
+        result.put('isError',true)
+        String outPut
+        StudentDetails studentDetails = StudentDetails.get(id)
+        if(studentDetails) {
+            try {
+                studentDetails.delete(flush:true)
+                result.put('isError',false)
+                result.put('message',"Student deleted successfully.")
+                outPut = result as JSON
+                render outPut
+                return
+
+            }
+
+            catch(DataIntegrityViolationException e) {
+                result.put('isError',true)
+                result.put('message',"Student could not deleted. Already in use.")
+                outPut = result as JSON
+                render outPut
+                return
+            }
+
+        }
+        result.put('isError',true)
+        result.put('message',"Class not found")
+        outPut = result as JSON
+        render outPut
+        return
+    }
+
+    def list() {
+        LinkedHashMap gridData
+        String result
+        LinkedHashMap resultMap =studentDetailsService.studentDetailsPaginateList(params)
+
+        if(!resultMap || resultMap.totalCount== 0){
+            gridData = [iTotalRecords: 0, iTotalDisplayRecords: 0, aaData: null]
+            result = gridData as JSON
+            render result
+            return
+        }
+        int totalCount =resultMap.totalCount
+        gridData = [iTotalRecords: totalCount, iTotalDisplayRecords: totalCount, aaData: resultMap.results]
+        result = gridData as JSON
+        render result
+
+    }
+
+    def edit(Long id) {
+        if (!request.method == 'POST') {
+            redirect(action: 'index')
+            return
+        }
+        LinkedHashMap result = new LinkedHashMap()
+        result.put('isError',true)
+        String outPut
+        StudentDetails studentDetails = StudentDetails.read(id)
+        if (!studentDetails) {
+            result.put('message','Class name not found')
+            outPut = result as JSON
+            render outPut
+            return
+        }
+        result.put('isError',false)
+        result.put('obj',studentDetails)
+        outPut = result as JSON
+        render outPut
+    }
+
+}
+
+class StudentDetailsCommand {
+    Long id
+    String name
+    String description
+
+    static constraints = {
+        name nullable: false
+    }
+}
---
+++ grails-app/services/com/school/app/StudentDetailsService.groovy
@@ -0,0 +1,57 @@
+package com.school.app
+
+import com.app.school.stmgmt.StudentDetails
+import grails.transaction.Transactional
+import org.codehaus.groovy.grails.web.servlet.mvc.GrailsParameterMap
+
+@Transactional
+class StudentDetailsService {
+
+    static final String[] sortColumns = ['id','name']
+    LinkedHashMap studentDetailsPaginateList(GrailsParameterMap params){
+        int iDisplayStart = params.iDisplayStart ? params.getInt('iDisplayStart') : CommonUtils.DEFAULT_PAGINATION_START
+        int iDisplayLength = params.iDisplayLength ? params.getInt('iDisplayLength') : CommonUtils.DEFAULT_PAGINATION_LENGTH
+        String sSortDir = params.sSortDir_0 ? params.sSortDir_0 : CommonUtils.DEFAULT_PAGINATION_SORT_ORDER
+        int iSortingCol = params.iSortingCols ? params.getInt('iSortingCols') : CommonUtils.DEFAULT_PAGINATION_SORT_IDX
+        //Search string, use or logic to all fields that required to include
+        String sSearch = params.sSearch ? params.sSearch : null
+        if (sSearch) {
+            sSearch = CommonUtils.PERCENTAGE_SIGN + sSearch + CommonUtils.PERCENTAGE_SIGN
+        }
+        String sortColumn = CommonUtils.getSortColumn(sortColumns,iSortingCol)
+        List dataReturns = new ArrayList()
+        def c = StudentDetails.createCriteria()
+        def results = c.list(max: iDisplayLength, offset: iDisplayStart) {
+            and {
+                eq("schoolId", CommonUtils.DEFAULT_SCHOOL_ID)
+            }
+            if (sSearch) {
+                or {
+                    ilike('firstName', sSearch)
+                    ilike('lastName', sSearch)
+                    ilike('nickName', sSearch)
+                    ilike('email', sSearch)
+                    ilike('cardNo', sSearch)
+                }
+            }
+            order(sortColumn, sSortDir)
+        }
+        int totalCount = results.totalCount
+        int serial = iDisplayStart;
+        if (totalCount > 0) {
+            if (sSortDir.equals(CommonUtils.SORT_ORDER_DESC)) {
+                serial = (totalCount + 1) - iDisplayStart
+            }
+            results.each { StudentDetails studentDetails ->
+                if (sSortDir.equals(CommonUtils.SORT_ORDER_ASC)) {
+                    serial++
+                } else {
+                    serial--
+                }
+                dataReturns.add([DT_RowId: studentDetails.id, 0: serial, 1: studentDetails.studentID,2:studentDetails.fullName, 3: studentDetails.email,4:studentDetails.cardNo,5:''])
+            }
+        }
+        return [totalCount:totalCount,results:dataReturns]
+    }
+
+}
