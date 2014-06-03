d3689592064fc9678e74282ef055dc7df21bae27
aminul <aminul2050@gmail.com>
2014-06-03 20:11:25 +0600

    school

--- grails-app/domain/com/app/school/settings/Subject.groovy
+++ grails-app/domain/com/app/school/settings/Subject.groovy
@@ -9,31 +9,39 @@ class Subject {
     Boolean compulsory =true
 
     static constraints = {
-    name nullable: false, unique: true
-    description nullable: true
+        name nullable: false, unique: true
+        description nullable: true
 
-    //System inserted data
-    schoolId nullable: true
-    createdBy nullable: true
-    createdOn nullable: true
-    lastUpdatedBy nullable: true
-    lastUpdatedOn nullable: true
-}
+        //System inserted data
+        schoolId nullable: true
+        createdBy nullable: true
+        createdOn nullable: true
+        lastUpdatedBy nullable: true
+        lastUpdatedOn nullable: true
+    }
+    static transients = ['subjectName']
 
-//common properties for every table
-Long schoolId
-String createdBy
-Date createdOn
-String lastUpdatedBy
-Date lastUpdatedOn
-def beforeInsert(){
-    schoolId=10000
-    createdBy = springSecurityService?.principal? springSecurityService.principal.username:'SystemUser'
-    createdOn = new Date()
-}
-def beforeUpdate() {
-    lastUpdatedBy = springSecurityService?.principal? springSecurityService.principal.username:'SystemUser'
-    lastUpdatedOn = new Date()
-}
-//common properties for every table
+    //common properties for every table
+    String subjectName
+    Long schoolId
+    String createdBy
+    Date createdOn
+    String lastUpdatedBy
+    Date lastUpdatedOn
+    def beforeInsert(){
+        schoolId=10000
+        createdBy = springSecurityService?.principal? springSecurityService.principal.username:'SystemUser'
+        createdOn = new Date()
+    }
+    def beforeUpdate() {
+        lastUpdatedBy = springSecurityService?.principal? springSecurityService.principal.username:'SystemUser'
+        lastUpdatedOn = new Date()
+    }
+    //common properties for every table
+    String getSubjectName(){
+        if(this.compulsory){
+            return this.name
+        }
+        return this.name+" (Optional)"
+    }
 }
